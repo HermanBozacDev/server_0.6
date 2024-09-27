3e3dcb21fafee3ccb5a84a0a8a143aa5d5932146
HermanBozacDev <martinhermanbozac@gmail.com>
2024-09-27 02:57:34 -0300

    Update notificationRoutes.js

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -25,6 +25,7 @@ router.post('/', async (req, res) => {
             console.log('[POST] /notifications - Notificación de tipo "payment" con ID:', paymentId);
 
             try {
+                console.log(req.body["id"])
                 // Usa el SDK de Mercado Pago para obtener los detalles del pago
                 const paymentDetails = await payment.get(paymentId); // Cambiado a `payment.get(paymentId)`
                 console.log('[POST] /notifications - Detalles del pago obtenidos:', paymentDetails);
