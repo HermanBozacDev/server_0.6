783d8a419c3471496bb80576ea1a9e00da8a1147
HermanBozacDev <martinhermanbozac@gmail.com>
2024-09-27 01:07:35 -0300

    Update eventos.js

--- models/eventos.js
+++ models/eventos.js
@@ -3,10 +3,7 @@ import mongoose from 'mongoose';
 const eventoSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
   fecha: { type: Date, required: true },
-  lugar: { type: String, required: true },
-  descripcion: { type: String },
-  precio: { type: Number, required: true },
-  capacidad: { type: Number, required: true }
+  descripcion: { type: String, required: true },
 });
 
 // Crea el modelo para la colección 'eventos'
