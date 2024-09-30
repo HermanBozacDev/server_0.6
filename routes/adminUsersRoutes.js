d875337c1200a80050d7615d80fb36d05758fb75
HermanBozacDev <martinhermanbozac@gmail.com>
2024-09-30 15:51:57 -0300

    Update adminUsersRoutes.js

--- routes/adminUsersRoutes.js
+++ routes/adminUsersRoutes.js
@@ -1,1 +1,16 @@
-asd
+
+import Admin from '../models/admin.js'; // Modelo de usuario
+
+
+const router = express.Router();
+
+
+router.get('/adminUsers', async (req, res) => {
+  try {
+    const users = await Admin.find(); // Obtener todos los usuarios del modelo Admin
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('[GET] /adminUsers - Error al obtener usuarios:', error);
+    res.status(500).json({ message: 'Error al obtener usuarios', error });
+  }
+});
