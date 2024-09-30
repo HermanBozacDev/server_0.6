800ca430ce4d4569d9088a34d0a18b6e2d87c269
HermanBozacDev <martinhermanbozac@gmail.com>
2024-09-30 19:46:12 -0300

    Update admin.js

--- models/admin.js
+++ models/admin.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const adminSchema = new mongoose.Schema({
   username: { type: String, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  role: { type: String, required: true, enum: ['superadmin', 'producer'] } // Campo de rol sin default
 });
 
 // Crea el modelo para la colección 'superadmin'
