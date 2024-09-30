e63cd5efaa82cb0168a2488055d3acd632073e40
HermanBozacDev <martinhermanbozac@gmail.com>
2024-09-30 15:17:46 -0300

    Update admin.js

--- models/admin.js
+++ models/admin.js
@@ -1,1 +1,11 @@
-asd
+import mongoose from 'mongoose';
+
+const adminSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true }
+});
+
+// Crea el modelo para la colección 'superadmin'
+const Admin = mongoose.model('Admin', adminSchema, 'superadmin');
+
+export default Admin;
