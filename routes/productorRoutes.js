9f6e5d374ac4c384b6bfc2d978b71a5f8a935aa0
HermanBozacDev <martinhermanbozac@gmail.com>
2024-10-02 17:11:55 -0300

    Update productorRoutes.js

--- routes/productorRoutes.js
+++ routes/productorRoutes.js
@@ -4,20 +4,20 @@ import { verifySuperAdminToken } from '../middlewares/auth.js'; // Importamos el
 const router = express.Router();
 
 router.get('/', verifySuperAdminToken, async (req, res) => {
-  console.log('[GET] /adminUsers - Solicitud recibida');
+  console.log('[GET] /productorUsers - Solicitud recibida');
   
   try {
     const users = await Productor.find(); // Obtener todos los usuarios del modelo Admin
-    console.log('[GET] /adminUsers - Usuarios encontrados:', users); // Log para verificar los usuarios encontrados
+    console.log('[GET] /productorUsers - Usuarios encontrados:', users); // Log para verificar los usuarios encontrados
     
     if (users.length === 0) {
-      console.log('[GET] /adminUsers - No se encontraron usuarios');
+      console.log('[GET] /productorUsers - No se encontraron usuarios');
       return res.status(200).json({ message: 'No hay usuarios disponibles' });
     }
 
     res.status(200).json(users);
   } catch (error) {
-    console.error('[GET] /adminUsers - Error al obtener usuarios:', error);
+    console.error('[GET] /productorUsers - Error al obtener usuarios:', error);
     res.status(500).json({ message: 'Error al obtener usuarios', error });
   }
 });
