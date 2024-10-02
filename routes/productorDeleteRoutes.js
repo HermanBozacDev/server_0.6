ed1b65171d45acf70a49d041fcd220b47ecda2cf
HermanBozacDev <martinhermanbozac@gmail.com>
2024-10-02 17:57:59 -0300

    Update productorDeleteRoutes.js

--- routes/productorDeleteRoutes.js
+++ routes/productorDeleteRoutes.js
@@ -5,23 +5,23 @@ import { verifySuperAdminToken } from '../middlewares/auth.js';
 const router = express.Router();
 
 /**
- * DELETE /producers/:id
+ * DELETE /producers
  * Ruta para eliminar un usuario productor. 
  */
-router.delete('/:id', verifySuperAdminToken, async (req, res) => {
-  const { id } = req.params;
+router.delete('/', verifySuperAdminToken, async (req, res) => {
+  const { id } = req.body; // Obtener el ID del cuerpo de la solicitud
 
   try {
     const deletedUser = await Productor.findByIdAndDelete(id);
     if (!deletedUser) {
-      console.warn("[DELETE] /producers/:id - Usuario no encontrado:", id);
+      console.warn("[DELETE] /producers - Usuario no encontrado:", id);
       return res.status(404).json({ message: 'Usuario no encontrado' });
     }
 
-    console.log("[DELETE] /producers/:id - Usuario eliminado:", id);
+    console.log("[DELETE] /producers - Usuario eliminado:", id);
     res.status(200).json({ message: 'Usuario eliminado exitosamente' });
   } catch (error) {
-    console.error("[DELETE] /producers/:id - Error al eliminar usuario:", error);
+    console.error("[DELETE] /producers - Error al eliminar usuario:", error);
     res.status(500).json({ message: 'Error al eliminar usuario', error });
   }
 });
