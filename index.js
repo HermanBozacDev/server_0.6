7964b74a9081d3119bea80b7977dfd98bd4d7a3d
HermanBozacDev <martinhermanbozac@gmail.com>
2024-10-03 01:52:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,19 +8,21 @@ import connectDB from './database/database.js';
 import notificationRoutes from './routes/notificationRoutes.js';
 import successRoutes from './routes/successRoutes.js';
 import qrRoutes from './routes/qrRoutes.js';
-import pendingRoutes from './routes/pendingRoutes.js';
-import failureRoutes from './routes/failureRoutes.js';
-import preferenceRoutes from './routes/preferenceRoutes.js';// Endpoint para registrar usuarios
 import eventosRoutes from './routes/eventosRoutes.js';
 import panelRoutes from './routes/panelProductorRoutes.js';
 
-
+//RUTAS DE MERCADO PAGO
+import preferenceRoutes from './routes/preferenceRoutes.js';
+import pendingRoutes from './routes/pendingRoutes.js';
+import failureRoutes from './routes/failureRoutes.js';
+import successRoutes from './routes/successRoutes.js';
 
 //RUTAS DE PRODUCTORES
 import productorRegister from './routes/productor/productorRegisterRoutes.js';
 import productorLogin    from './routes/productor/productorLoginRoutes.js';
 import productorUsers    from './routes/productor/productorUsersRoutes.js';
 import productorDelete   from './routes/productor/productorDeleteRoutes.js';
+
 //RUTAS DE LA ADMINISTRACION
 import adminRegister from './routes/admin/adminRegisterRoutes.js';
 import adminLogin    from './routes/admin/adminLoginRoutes.js';
@@ -41,18 +43,22 @@ console.log(client)
 app.use(express.json());
 app.use(configureCors());
 app.use('/api/notifications', notificationRoutes);
-app.use('/api/success', successRoutes);
 app.use('/api/display-qr', qrRoutes);
-app.use('/api/pending', pendingRoutes);
-app.use('/api/failure', failureRoutes);
-app.use('/api/create_preference', preferenceRoutes);
 app.use('/api/panelAdminEvento', panelRoutes);
 app.use('/api/eventos', eventosRoutes);
+
+//RUTAS DE MERCADO PAGO
+app.use('/api/create_preference', preferenceRoutes);
+app.use('/api/pending', pendingRoutes);
+app.use('/api/failure', failureRoutes);
+app.use('/api/success', successRoutes);
+
 //RUTAS DE PRODUCTORES
 app.use('/api/productorRegister', productorRegister);
 app.use('/api/productorLogin',productorLogin);
 app.use('/api/productorUsers', productorUsers);
 app.use('/api/productorDelete', productorDelete);
+
 //RUTAS DE ADMINISTRACION
 app.use('/api/adminRegister', adminRegister);
 app.use('/api/adminLogin', adminLogin);
