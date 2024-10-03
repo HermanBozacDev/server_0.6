51a42e49f1cb09a15cc27adb4629baefd9a7ade4
HermanBozacDev <martinhermanbozac@gmail.com>
2024-10-03 17:37:58 -0300

    Update preferenceRoutes.js

--- routes/mercadopago/preferenceRoutes.js
+++ routes/mercadopago/preferenceRoutes.js
@@ -34,7 +34,7 @@ router.post("/", async (req, res) => {
 
 
         const preference = new Preference(client); // Verifica que `client` esté correctamente definido
-        const result = await preference.create({body: paymentData,headers:{}});
+        const result = await preference.create({body: paymentData,headers:{'X-Integrator-Id': 'dev_24c65fb163bf11ea96500242ac130004',}});
 
 
         // Log de éxito
