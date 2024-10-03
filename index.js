f7dd201d000105a685651b62b8378a0e8358eedd
HermanBozacDev <martinhermanbozac@gmail.com>
2024-10-03 01:09:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,22 +11,17 @@ import qrRoutes from './routes/qrRoutes.js';
 import pendingRoutes from './routes/pendingRoutes.js';
 import failureRoutes from './routes/failureRoutes.js';
 import preferenceRoutes from './routes/preferenceRoutes.js';// Endpoint para registrar usuarios
-
 import eventosRoutes from './routes/eventosRoutes.js';
+import panelRoutes from './routes/panelProductorRoutes.js';
+
 
 
 //RUTAS DE PRODUCTORES
-import panelRoutes from './routes/panelProductorRoutes.js';
-
 import productorRegister from './routes/productor/productorRegisterRoutes.js';
 import productorLogin    from './routes/productor/productorLoginRoutes.js';
 import productorUsers    from './routes/productor/productorUsersRoutes.js';
 import productorDelete   from './routes/productor/productorDeleteRoutes.js';
-
-
 //RUTAS DE LA ADMINISTRACION
-
-
 import adminRegister from './routes/admin/adminRegisterRoutes.js';
 import adminLogin    from './routes/admin/adminLoginRoutes.js';
 import adminUsers    from './routes/admin/adminUsersRoutes.js';
@@ -51,16 +46,13 @@ app.use('/api/display-qr', qrRoutes);
 app.use('/api/pending', pendingRoutes);
 app.use('/api/failure', failureRoutes);
 app.use('/api/create_preference', preferenceRoutes);
-
 app.use('/api/panelAdminEvento', panelRoutes);
 app.use('/api/eventos', eventosRoutes);
-
 //RUTAS DE PRODUCTORES
 app.use('/api/productorRegister', productorRegister);
 app.use('/api/productorLogin',productorLogin);
 app.use('/api/productorUsers', productorUsers);
 app.use('/api/productorDelete', productorDelete);
-
 //RUTAS DE ADMINISTRACION
 app.use('/api/adminRegister', adminRegister);
 app.use('/api/adminLogin', adminLogin);
