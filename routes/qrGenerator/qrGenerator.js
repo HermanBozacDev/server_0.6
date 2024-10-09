9049bbcbe6ff90d61a71d88ee5babf4b17e6f95d
HermanBozacDev <martinhermanbozac@gmail.com>
2024-10-09 12:04:32 -0300

    Update qrGenerator.js

--- routes/qrGenerator/qrGenerator.js
+++ routes/qrGenerator/qrGenerator.js
@@ -1,1 +1,19 @@
+// qrCodeService.js
+import express from 'express';
+import QRCode from 'qrcode';
 
+const router = express.Router();
+
+router.post("/generarQR", async (req, res) => {
+    const ticketData = req.body;
+
+    try {
+        const qrCodeImageUrl = await QRCode.toDataURL(JSON.stringify(ticketData));
+        res.status(200).json({ qrCodeImageUrl });
+    } catch (error) {
+        console.error("Error generando el código QR:", error);
+        res.status(500).json({ error: "Error generando el código QR" });
+    }
+});
+
+export default router;
