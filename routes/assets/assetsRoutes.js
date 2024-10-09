d8641c5de13ec4d0c052a593387b62feb45f4f5a
HermanBozacDev <martinhermanbozac@gmail.com>
2024-10-09 16:23:00 -0300

    Update assetsRoutes.js

--- routes/assets/assetsRoutes.js
+++ routes/assets/assetsRoutes.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import multer from 'multer';
-import path from 'path'; // Asegúrate de importar path para manejar extensiones de archivos
+import path from 'path';
 const router = express.Router();
 
 // Configuración de multer para almacenar archivos
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, '/uploads'); // Directorio donde se guardarán las imágenes
+    cb(null, '/uploads'); // Asegúrate de que este directorio exista y sea accesible
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, uniqueSuffix + path.extname(file.originalname)); // Nombres únicos para evitar colisiones
+    cb(null, uniqueSuffix + path.extname(file.originalname));
   },
 });
 
@@ -18,11 +18,12 @@ const upload = multer({ storage: storage });
 
 // Ruta para subir imágenes
 router.post('/', upload.single('image'), (req, res) => {
-  // req.file contiene información sobre el archivo subido
+  console.log('Recibiendo archivo de imagen...'); // Mensaje al iniciar la recepción
   if (req.file) {
-    console.log('Imagen subida:', req.file); // Agregar el console.log aquí
+    console.log('Imagen subida:', req.file); // Información sobre la imagen
     res.status(200).json({ message: 'Imagen subida con éxito', filePath: `/uploads/${req.file.filename}` });
   } else {
+    console.log('Error: No se recibió ningún archivo'); // Mensaje de error
     res.status(400).json({ message: 'Error al subir la imagen' });
   }
 });
