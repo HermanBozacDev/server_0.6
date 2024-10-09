5df7d423dc149bebfabb62dc1e969e66f31abbed
HermanBozacDev <martinhermanbozac@gmail.com>
2024-10-09 19:39:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -33,11 +33,15 @@ import uploadImage from './routes/assets/assetsRoutes.js';
 import express from "express";
 import multer from 'multer';
 import path from 'path';
+
+
 //DATABASES
 connectDB();
 //CONSTANTES
+
 const app = express();
 const port = appConfig.port;
+const __dirname = path.dirname(__filename);
 //APPS
 app.use(express.json());
 app.use(configureCors());
